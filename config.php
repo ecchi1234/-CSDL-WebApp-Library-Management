d091c66f0ddd5b015efa8ef26c5023279602a98f
ecchi1234 <62091560+ecchi1234@users.noreply.github.com>
2020-04-15 01:17:59 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'us-cdbr-iron-east-01.cleardb.net');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'b47d7d31f530ec');
+define('DB_USERNAME', 'b47d7d31f530ec');
+define('DB_PASSWORD', '2593df18');
 define('DB_NAME', 'heroku_2b7e9ade604dde8');
  
 /* Attempt to connect to MySQL database */
