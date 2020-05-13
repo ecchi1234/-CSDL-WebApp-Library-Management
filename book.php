4a99b22aad8ac0665fd4a321d8b32132948de92f
ecchi1234 <48946464+ecchi1234@users.noreply.github.com>
2020-05-13 17:57:16 +0700

    update 4

--- book.php
+++ book.php
@@ -646,12 +646,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       <!--Hết modal delete book-->
 
       <!-- Bootstrap core JavaScript-->
-      <script src="./vendor/jquery/jquery.min.js"></script>
+      <!-- <script src="./vendor/jquery/jquery.min.js"></script> -->
       <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
       <!-- Core plugin JavaScript-->
       <script src="./vendor/jquery-easing/jquery.easing.min.js"></script>
       <!-- Custom scripts for all pages-->
-      <script src="./js/sb-admin-2.min.js"></script>
+      <!-- <script src="./js/sb-admin-2.min.js"></script> -->
       <!-- Page level plugins -->
       <script src="./vendor/datatables/jquery.dataTables.min.js"></script>
       <script src="./vendor/datatables/dataTables.bootstrap4.min.js"></script>
